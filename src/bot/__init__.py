0ca4a56b4c256e5fcb38e382a5a30c88cb4e2812
guff192 <kochkons1@ya.ru>
2022-09-18 21:04:18 +0400

    added redis storage

--- src/bot/__init__.py
+++ src/bot/__init__.py
@@ -1,6 +1,7 @@
 from asyncio import get_event_loop
 
 from aiogram import Bot, Dispatcher, types
+# FIXME (uncomment): from aiogram.contrib.fsm_storage.redis import RedisStorage
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from src.bot.middlewares.logging import StateLoggingMiddleware
@@ -8,9 +9,10 @@ from src.core import settings
 from .handlers import register_all_handlers 
 
 
-# TODO storage = RedisStorage()
 bot = Bot(settings.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
-dp = Dispatcher(bot, storage=MemoryStorage(), loop=get_event_loop())
+# TODO storage = RedisStorage()
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage, loop=get_event_loop())
 dp.middleware.setup(StateLoggingMiddleware())
 
 register_all_handlers(dp)
